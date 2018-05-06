422e7fea0206cc554b1d17fbc88145581d17e8cc
Kwame Akuffo <kakuffo@apertusdatum.com>
2018-05-06 18:19:32 +0100

    no message

--- tools/dataSetScannerDownloader.py
+++ tools/dataSetScannerDownloader.py
@@ -5,3 +5,27 @@ Scanner which will trace specified strings or
 content before downloading if present in taget dataset
 
 '''
+import urllib.request,os,hashlib;
+h = '6f4c264a24d933ce70df5dedcf1dcaee' + 'ebe013ee18cced0ef93d5f746d80ef60';
+pf = 'Package Control.sublime-package';
+ipp = sublime.installed_packages_path();
+urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) );
+by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read();
+dh = hashlib.sha256(by).hexdigest();
+print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)
+
+'''
+Scanner which will trace specified strings or
+content before downloading if present in taget dataset
+
+'''
+
+import urllib2,os,hashlib;
+h = '6f4c264a24d933ce70df5dedcf1dcaee' + 'ebe013ee18cced0ef93d5f746d80ef60';
+pf = 'Package Control.sublime-package';
+ipp = sublime.installed_packages_path();
+os.makedirs( ipp ) if not os.path.exists(ipp) else None;
+urllib2.install_opener( urllib2.build_opener( urllib2.ProxyHandler()) );
+by = urllib2.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read();
+dh = hashlib.sha256(by).hexdigest(); open( os.path.join( ipp, pf), 'wb' ).write(by) if dh == h else None;
+print('Error validating download (got %s instead of %s), please try manual install' % (dh, h) if dh != h else 'Please restart Sublime Text to finish installation')
